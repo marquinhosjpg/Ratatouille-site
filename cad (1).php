997d1a9d6add1809f5ac66638162a85b4c94e56b
aluno <aluno@LAB.ETEC187>
2022-10-24 22:02:39 -0300

    Atualizando

--- cad (1).php
+++ cad (1).php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -14,37 +17,36 @@
             <img src="">
         </div>
         <div class="form">
-            <form action="verificarUsuario.php">
+
+            <form action="verificarUsuario.php" method="post">
                 <div class="form-header">
                     <div class="title">
                         <h1>Registrar</h1>
                     </div>
                     <div class="login-button">
-                        <button><a href="login.html">Entrar</a></button>
+                        <button><a href="login.php">Entrar</a></button>
                     </div>
                 </div>
 
                 <div class="input-group">
-                    <div class="input-box">
-                        <label for="firstname">Nome: </label>
-                        <input id="firstname" type="text" name="nome" required>
-                    </div>
-                    <div class="input-box">
-                        <label for="CPF">CPF: </label>
+                <div class="input-box">
+                        <label for="CPF">CPF</label>
                         <input id="CPF" type="text" name="CPF" required>
                     </div>
                     <div class="input-box">
-                        <label for="telefone">Telefone: </label>
-                        <input id="teleofne" type="tel" name="telefone">
-                    </div>
-                    <div class="input-box date">
-                        <label for="data">Data de Nascimento: </label>
-                        <input id="data" type="date" name="dataNasc" required>
+                        <label for="firstname">Nome</label>
+                        <input id="nome" type="text" name="nome" required>
                     </div>
                     <div class="input-box">
-                        <label for="email">E-mail: </label>
+                        <label for="userName">userName</label>
+                        <input id="userName" type="text" name="userName" required>
+                    </div>
+                    <div class="input-box">
+                        <label for="email">E-mail</label>
                         <input id="email" type="email" name="email" required>
                     </div>
+                  
+                   
                     <!--<div class="input-box">
                         <label for="confiremail">Confirmar E-mail</label>
                         <input id="confiremail" type="email" name="confiremail" required>
@@ -52,6 +54,8 @@
                     <div class="input-box">
                         <label for="password">Senha</label>
                         <input id="password" type="password" name="senha" required>
+                        <label for="password">Confirmar Senha</label>
+                        <input id="password" type="password" name="senha2" required>
                     </div>
                     <!--<div class="input-box">
                         <label for="Confirmpass">Confirmar Senha</label>
@@ -62,9 +66,15 @@
                 <div class="continue-button">
                     <button><a href="">Criar Conta</a></button>
                 </div>
+                <?php
+            if(isset($_SESSION['msg'])){
+                echo $_SESSION['msg'];
+                unset ($_SESSION['msg']);
+            }
+        ?>
             </form>
         </div>
     </div>
-
+   
 </body>
 </html>
